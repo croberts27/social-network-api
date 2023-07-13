aaef6f2c268b49dcd30ee94e020ab3e58cc5708e
Calvin Roberts <you@example.com>
2023-07-12 21:45:06 -0600

    finished thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const thoughtSchema = new mongoose.Schema({
   thoughtText: {
     type: String,
     required: true,
     validate: {
-      validator: function(v) {
+      validator: function (v) {
         return v.length >= 1 && v.length <= 280;
       },
-      message: props => `${props.value} must be between 1 and 280 characters!`
-    }
-  }
+      message: (props) =>
+        `${props.value} must be between 1 and 280 characters!`,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function (v) {
+      //formatting timestamp using a getter method
+      return new Date(v).toLocaleString();
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reaction" }],
 });
 
-const Thought = mongoose.model('Thought', thoughtSchema);
+// virtual property called 'reactionCount' that retrieves the length of the thought's 'reactions' array field on query
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+const handleError = (err) => console.log(err);
+
+// Create new instance of the model
+
+Thought.create({
+  thoughtText: "This is my first thought!",
+  username: "jman777",
+})
+  .then((result) => console.log("Thought has been posted!", result))
+  .catch((err) => handleError(err));
+
+module.exports = Thought;
