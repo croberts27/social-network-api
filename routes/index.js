23ce9e4a15ed8471830b0ee6dd35fa28f866636a
Calvin Roberts <you@example.com>
2023-07-16 13:09:19 -0600

    made updates to index.js in /routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+//require express router
 const router = require("express").Router();
+//require all api routes
 const apiRoutes = require("./api");
-
+// addes '/api' to all route endpoints
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  return res.send("Wrong route!");
+  res.status(404).send("Wrong route!");
 });
 
 module.exports = router;
