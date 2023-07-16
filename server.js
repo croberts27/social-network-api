03dfe5ac20cd1811dd4a68084f8c45df82084e39
Calvin Roberts <you@example.com>
2023-07-16 13:06:36 -0600

    updates to server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const db = require("./config/connection.js");
-const routes = require("./routes/index.js");
-// Require model
-const { User, Thought, Reaction } = require("./models");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.use(routes);
 
 db.on("error", (error) => {
   console.error("Error connecting to MongoDB:", error);
